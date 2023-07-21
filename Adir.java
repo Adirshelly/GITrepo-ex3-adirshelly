9396ea53039bf7edbd6610bee00769cd42ae6d01
Adirshelly <adirshelly92@gmail.com>
2023-07-21 15:54:26 +0300

    First commit of the two files

---
+++ Adir.java
@@ -0,0 +1,8 @@
+public class Adir { 
+	public static void main(String[] args) { 
+		// for loop that prints "Adir" ten times.  
+		for (int i = 0; i < 10; i++) { 
+			System.out.println("Adir"); 
+		} 
+	} 
+} 
